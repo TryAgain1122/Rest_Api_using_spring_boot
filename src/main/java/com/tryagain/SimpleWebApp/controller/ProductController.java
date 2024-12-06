651f44b2a3bdcd66b04dddc1f39ca58160a6c658
TryAgain1122 <luisrafhael1122@gmail.com>
2024-12-07 03:15:31 +0800

    Initial commit

---
+++ src/main/java/com/tryagain/SimpleWebApp/controller/ProductController.java
@@ -0,0 +1,22 @@
+package com.tryagain.SimpleWebApp.controller;
+
+
+import com.tryagain.SimpleWebApp.model.Product;
+import com.tryagain.SimpleWebApp.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductService service;
+
+    @RequestMapping("/products")
+    public List<Product> getProducts () {
+        return service.getProducts();
+    }
+}
---
+++ src/main/java/com/tryagain/SimpleWebApp/service/ProductService.java
@@ -0,0 +1,21 @@
+package com.tryagain.SimpleWebApp.service;
+
+import com.tryagain.SimpleWebApp.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    List<Product> products = Arrays.asList(
+            new Product(101, "Iphone", 50000),
+            new Product(102, "Camera", 7000),
+            new Product(103, "Samsung", 70000)
+    );
+
+    public List<Product> getProducts() {
+        return  products;
+    }
+}
